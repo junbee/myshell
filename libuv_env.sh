d3f055bc5e847d89d1689680fe415d0b39e99e46
mytutorials <junbee@163.com>
2018-01-29 19:27:40 +0800

    Update libuv_env.sh

--- libuv_env.sh
+++ libuv_env.sh
@@ -1,3 +1,6 @@
+echo "begin libuv_env.sh"
+cd ~
+
 yum -y install autoconf  automake  libtool  make   &&  \
 wget https://github.com/libuv/libuv/archive/v1.19.1.tar.gz  &&  tar -xzf v1.19.1.tar.gz  &&  \
 cd libuv-1.19.1   &&   sh autogen.sh   &&   ./configure  &&  make  &&  make install
@@ -8,3 +11,6 @@ wget https://raw.githubusercontent.com/junbee/myshell/master/libuv_helloworld.c
 gcc libuv_helloworld.c -o libuv_helloworld.exe  -L/usr/local/lib -luv  -Wl,-rpath=/usr/local/lib
 chmod 755  libuv_helloworld.exe
 ./libuv_helloworld.exe
+
+echo "end libuv_env.sh"
+cd ~
