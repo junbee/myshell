83a525908f59d70079a8e753a135b9b375f16c34
mytutorials <junbee@163.com>
2018-02-10 15:47:54 +0800

    Update gtk3_helloworld.c

--- gtk3_helloworld.c
+++ gtk3_helloworld.c
@@ -11,6 +11,7 @@ int main (int  argc, char *argv[])
 
     g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_widget_show (window);
 
     gtk_main ();
