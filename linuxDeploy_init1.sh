864485ad8b841ef91e0bede8af70a336f9acfe65
mytutorials <junbee@163.com>
2018-11-10 16:22:47 +0800

    Create linuxDeploy_init1.sh

---
+++ linuxDeploy_init1.sh
@@ -0,0 +1,16 @@
+
+sudo apt-get  -y    install  tmux
+
+#升级 VIM
+sudo apt-get  -y    install   software-properties-common python-software-properties
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt update
+sudo apt install vim
+
+
+
+
+
+
+
+
